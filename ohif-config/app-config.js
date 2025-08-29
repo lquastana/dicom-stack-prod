0ed486252e714ca636f0fd4eb77f9c4ff1cf419d
Laurent Quastana <laurent.quastana@gmail.com>
2025-08-29 13:32:53 +0200

    Decompress compressed dicoms before sending

---
+++ ohif-config/app-config.js
@@ -0,0 +1,20 @@
+window.config = {
+  routerBasename: '/',
+  showStudyList: true,
+  servers: {
+    dicomWeb: [
+      {
+        name: 'Orthanc',
+        wadoUriRoot: '/orthanc/wado',
+        qidoRoot: '/orthanc/dicom-web',
+        wadoRoot: '/orthanc/dicom-web',
+        qidoSupportsIncludeField: true,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        enableStudyLazyLoad: true,
+        supportsFuzzyMatching: true,
+        dicomUploadEnabled: true
+      }
+    ]
+  }
+};
